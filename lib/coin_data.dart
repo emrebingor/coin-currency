574ccb61ff4368862d986d6479dbc422beb15e56
emrebingor <emrebingor5@gmail.com>
2022-07-04 11:43:38 +0300

    coin data list addition

---
+++ lib/coin_data.dart
@@ -0,0 +1,38 @@
+
+
+const List<String> currencyList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> coinList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+
+
+class CoinData {
+
+
+}
